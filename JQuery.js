d620bbf9377a7544a6db66dd19faf601b5b7142a
Usama <Usamasadiq163@gmail.com>
2022-04-11 10:05:51 +0500

    dark done

--- JQuery.js
+++ JQuery.js
@@ -1,4 +1,4 @@
-$document().ready(function() {
+$(document).ready(function() {
     $('.navbar-nav').on('click', 'a', function() {
         $('.navbar-nav a.active').removeClass('active');
         $(this).addClass('active');
